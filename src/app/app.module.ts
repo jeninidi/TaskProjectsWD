bd08560d9c72485d79806f3d0050977a2bb06af8
Zheni Nikolaeva Dimitrova <zhen0101@stud.kea.dk>
2019-03-04 13:36:15 +0100

    tried to implement maps but didnt work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,24 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from "./MaterialModule";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// import {
+//   MapModule,
+//   MapAPILoader,
+//   MarkerTypeId,
+//   WindowRef,
+//   DocumentRef,
+//   MapServiceFactory,
+//   BingMapAPILoaderConfig,
+//   BingMapAPILoader,
+//   GoogleMapAPILoader,
+//   GoogleMapAPILoaderConfig,
+//   IBox,
+//   IMarkerIconInfo,
+//   IMapOptions,
+// } from 'angular-maps';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,14 +38,44 @@ import { TaskAboutComponent } from './task-list-assignment1/task-about/task-abou
     TaskListAssignment1Component,
     TaskDetailsComponent,
     TaskAboutComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    // MapModule.forRoot(),
+    // MapAPILoader,
+    // MarkerTypeId,
+    // IMapOptions,
+    // IBox,
+    // IMarkerIconInfo,
+    // WindowRef,
+    // DocumentRef,
+    // MapServiceFactory,
+    // BingMapAPILoaderConfig,
+    // BingMapAPILoader,
+    // GoogleMapAPILoader,
+    // GoogleMapAPILoaderConfig
+
+
   ],
-  providers: [],
+  providers: [
+    // {
+    //   provide: MapAPILoader, deps: [], useFactory: MapServiceProviderFactory,
+    
+    // }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+// export function MapServiceProviderFactory(){
+//   let bc: BingMapAPILoaderConfig = new BingMapAPILoaderConfig();
+//   bc.apiKey ="..."; // your bing map key
+//   bc.branch = "experimental"; 
+//       // to use the experimental bing brach. There are some bug fixes for
+//       // clustering in that branch you will need if you want to use 
+//       // clustering.
+//   return new BingMapAPILoader(bc, new WindowRef(), new DocumentRef());
+// }
