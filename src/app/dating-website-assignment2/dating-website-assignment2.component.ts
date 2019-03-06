f7a95f9d7729d2e7906b3de5516727c3a4d480d2
Zheni Nikolaeva Dimitrova <zhen0101@stud.kea.dk>
2019-03-06 13:38:51 +0100

    Revert "form doesnt work"
    This reverts commit ead91a1de038bd45beac3b7770e2325d81835d36.

--- src/app/dating-website-assignment2/dating-website-assignment2.component.ts
+++ src/app/dating-website-assignment2/dating-website-assignment2.component.ts
@@ -7,12 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatingWebsiteAssignment2Component implements OnInit {
 
- 
-
   constructor() { }
 
   ngOnInit() {
-    
   }
 
 }
